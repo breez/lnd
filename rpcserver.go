3e06d7ec4f77879589b70298e20f2ac6de75c5b9
Roei Erez <roeierez@gmail.com>
2022-07-28 16:41:16 +0300

    add dry run to SendCoins rpc

--- rpcserver.go
+++ rpcserver.go
@@ -984,7 +984,7 @@ func (r *rpcServer) Stop() error {
 	return nil
 }
 
-//MemDial returns a net.Conn for in-memory RPC
+// MemDial returns a net.Conn for in-memory RPC
 func MemDial() (net.Conn, error) {
 	if memoryRPCListener == nil {
 		return nil, errors.New("Memory RPC is not configured")
@@ -1346,6 +1346,7 @@ func (r *rpcServer) SendCoins(ctx context.Context,
 	}
 
 	var txid *chainhash.Hash
+	var txRaw []byte
 
 	wallet := r.server.cc.Wallet
 	maxFeeRate := r.cfg.Sweeper.MaxFeeRate.FeePerKWeight()
@@ -1472,22 +1473,35 @@ func (r *rpcServer) SendCoins(ctx context.Context,
 			return nil, err
 		}
 
-		rpcsLog.Debugf("Sweeping coins from wallet to addr=%v, "+
-			"with tx=%v", in.Addr, spew.Sdump(sweepTxPkg.SweepTx))
-
-		// As our sweep transaction was created, successfully, we'll
-		// now attempt to publish it, cancelling the sweep pkg to
-		// return all outputs if it fails.
-		err = wallet.PublishTransaction(sweepTxPkg.SweepTx, label)
-		if err != nil {
+		sweepTx := sweepTxPkg.SweepTx
+		if in.DryRun {
+			rpcsLog.Debugf("Sweeping all coins (dry run) from wallet to addr=%v, "+
+				"with tx=%v", in.Addr, spew.Sdump(sweepTx))
 			sweepTxPkg.CancelSweepAttempt()
+		} else {
+			rpcsLog.Debugf("Sweeping all coins from wallet to addr=%v, "+
+				"with tx=%v", in.Addr, spew.Sdump(sweepTx))
 
-			return nil, fmt.Errorf("unable to broadcast sweep "+
-				"transaction: %v", err)
+			// As our sweep transaction was created, successfully, we'll
+			// now attempt to publish it, cancelling the sweep pkg to
+			// return all outputs if it fails.
+			err = wallet.PublishTransaction(sweepTx, label)
+			if err != nil {
+				sweepTxPkg.CancelSweepAttempt()
+
+				return nil, fmt.Errorf("unable to broadcast sweep "+
+					"transaction: %v", err)
+			}
 		}
 
-		sweepTXID := sweepTxPkg.SweepTx.TxHash()
+		sweepTXID := sweepTx.TxHash()
 		txid = &sweepTXID
+		var buf bytes.Buffer
+		if err := sweepTx.Serialize(&buf); err != nil {
+			return nil, fmt.Errorf("unable to serialize sweep "+
+				"transaction: %v", err)
+		}
+		txRaw = buf.Bytes()
 	} else {
 
 		// We'll now construct out payment map, and use the wallet's
@@ -1515,7 +1529,7 @@ func (r *rpcServer) SendCoins(ctx context.Context,
 
 	rpcsLog.Infof("[sendcoins] spend generated txid: %v", txid.String())
 
-	return &lnrpc.SendCoinsResponse{Txid: txid.String()}, nil
+	return &lnrpc.SendCoinsResponse{Txid: txid.String(), Tx: txRaw}, nil
 }
 
 // SendMany handles a request for a transaction create multiple specified
