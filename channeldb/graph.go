edb4891e9066731ca58eb3df86b8e15852bfe406
Roei Erez <roeierez@gmail.com>
2022-10-13 22:39:40 +0300

    add ReloadCache method

--- channeldb/graph.go
+++ channeldb/graph.go
@@ -257,6 +257,31 @@ func NewChannelGraph(db kvdb.Backend, rejectCacheSize, chanCacheSize int,
 	return g, nil
 }
 
+func (c *ChannelGraph) ReloadCache() error {
+	err := c.ForEachNodeCacheable(
+		func(tx kvdb.RTx, node GraphCacheNode) error {
+			c.graphCache.AddNodeFeatures(node)
+
+			return nil
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	err = c.ForEachChannel(func(info *models.ChannelEdgeInfo,
+		policy1, policy2 *models.ChannelEdgePolicy) error {
+
+		c.graphCache.AddChannel(info, policy1, policy2)
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // channelMapKey is the key structure used for storing channel edge policies.
 type channelMapKey struct {
 	nodeKey route.Vertex
