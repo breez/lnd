53eb6a5b81afbf5e811459c90eb591db8a2e6c52
Jesse de Wit <witdejesse@hotmail.com>
2023-10-16 15:12:14 +0200

    skip dustlimit check if reserve is 0
    This check fails for core lightning nodes.
    They set a nonzero dustlimit when the reserve is zero.

--- lnwallet/reservation.go
+++ lnwallet/reservation.go
@@ -625,7 +625,7 @@ func (r *ChannelReservation) validateReserveBounds() bool {
 		maxDustLimit = theirDustLimit
 	}
 
-	if theirDustLimit == 0 && theirRequiredReserve == 0 {
+	if theirRequiredReserve == 0 {
 		minChanReserve = ourRequiredReserve
 		maxDustLimit = ourDustLimit
 	}
