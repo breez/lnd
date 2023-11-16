5ac1779cb7f38babc0bd8fe43b9c011b38b14e48
Yaacov Akiba Slama <ya@slamail.org>
2023-11-16 13:32:22 +0200

    Add a configuration parameter to customize the remote channel reserve
    Allow channel reserve below dust
    Accept a 0 channel reserve in the other side of channel

--- funding/manager.go
+++ funding/manager.go
@@ -4964,6 +4964,11 @@ func (f *Manager) handleInitFundingMsg(msg *InitFundingMsg) {
 		DustLimit: ourDustLimit,
 		CsvDelay:  remoteCsvDelay,
 	}
+	// If the other side accepts a 0 amount channel reserve, we use a
+	// dustLimit of 0.
+	if chanReserve == 0 {
+		commitParams.DustLimit = 0
+	}
 	err = lnwallet.VerifyConstraints(
 		bounds, commitParams, resCtx.maxLocalCsv, capacity,
 	)
