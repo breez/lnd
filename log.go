abf058589b67796dbde123a903e0c4534480fdc0
Jesse de Wit <witdejesse@hotmail.com>
2024-06-03 12:08:29 +0200

    localchans: recreate missing edge if not found
    If a node contains a channel, but doesn't have a corresponding edge in
    the graph database, updating the channel policy would fail. In this
    commit the edge is recreated if the channel exists. This ensures a node
    can recover from a missing edge in the graph database by calling
    updatechanpolicy.

--- log.go
+++ log.go
@@ -44,6 +44,7 @@ import (
 	"github.com/lightningnetwork/lnd/peernotifier"
 	"github.com/lightningnetwork/lnd/routing"
 	"github.com/lightningnetwork/lnd/routing/blindedpath"
+	"github.com/lightningnetwork/lnd/routing/localchans"
 	"github.com/lightningnetwork/lnd/rpcperms"
 	"github.com/lightningnetwork/lnd/signal"
 	"github.com/lightningnetwork/lnd/submarineswap"
@@ -169,6 +170,7 @@ func SetupLoggers(root *build.RotatingLogWriter, interceptor signal.Interceptor)
 	AddSubLogger(root, "PEER", interceptor, peer.UseLogger)
 	AddSubLogger(root, "CHCL", interceptor, chancloser.UseLogger)
 	AddSubLogger(root, "SUBS", interceptor, submarineswap.UseLogger)
+	AddSubLogger(root, "LCHN", interceptor, localchans.UseLogger)
 
 	AddSubLogger(root, routing.Subsystem, interceptor, routing.UseLogger)
 	AddSubLogger(root, routerrpc.Subsystem, interceptor, routerrpc.UseLogger)
